9081a74e2bbf75adf48d37c75783c63483aeb206
FlorentinoJunior <53458539+FlorentinoJunior@users.noreply.github.com>
2023-06-18 12:52:10 -0400

    haciendo caso de uso y agregando comentarios
    Caso de uso, donde se vea reflejado un flujo de trabajo, ingresando datos desde la consola, o a través
    de un archivo de texto de entrada.

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"github.com/FlorentinoJunior/proyectoGo.git/entities"
+)
+
 func main() {
-	// Aquí va tu código...
+	reader := bufio.NewReader(os.Stdin)
+
+	// Recoge el nombre del estudiante
+	fmt.Print("Ingrese el nombre del estudiante: ")
+	nombreEstudiante, _ := reader.ReadString('\n')
+
+	// Recoge el nombre y autor del libro
+	fmt.Print("Ingrese el título del libro: ")
+	tituloLibro, _ := reader.ReadString('\n')
+
+	fmt.Print("Ingrese el autor del libro: ")
+	autorLibro, _ := reader.ReadString('\n')
+
+	// Crea un nuevo estudiante
+	estudiante1 := entities.NewStudent(nombreEstudiante)
+
+	// Crea un nuevo libro
+	libro1 := entities.NewBook(tituloLibro, autorLibro)
+
+	// El estudiante toma prestado el libro
+	estudiante1.BorrowBook(libro1)
+
+	// Imprime el estado del libro después de ser prestado
+	fmt.Printf("Estado del libro después de ser prestado: %s\n", libro1.Estado)
+
+	// El estudiante devuelve el libro
+	estudiante1.ReturnBook(libro1)
+
+	// Imprime el estado del libro después de ser devuelto
+	fmt.Printf("Estado del libro después de ser devuelto: %s\n", libro1.Estado)
 }
